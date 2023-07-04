4d5c15426689d51f5250677ecf88b886e731a806
Brandon <93879641+beanziii@users.noreply.github.com>
2023-07-04 18:34:56 +0100

    rename to form

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -1,4 +1,4 @@
-import CreateBracketForm from "@/components/features/bracket/create-bracket-wizard";
+import CreateBracketForm from "@/components/features/bracket/create-bracket-form";
 import Header from "@/components/features/header/header";
 
 // TODO: Add middleware to protect this page if user in not logged in
