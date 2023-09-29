058dcf08947c822fbf7388891d966798188d4b8f
beanziii <93879641+beanziii@users.noreply.github.com>
2023-09-29 21:13:05 +0100

    clean up game code a bit

--- src/components/features/game/vote-game.tsx
+++ src/components/features/game/vote-game.tsx
@@ -28,7 +28,7 @@ export default function VoteGame({ bracket }: Props) {
     setCurrentMatchupIndex(0);
     setWinners([]);
     setCurrentRound((prevRound) => prevRound + 1);
-    moveToNextMatchup(0, newMatchups); // Skip initial double-byes
+    // moveToNextMatchup(0, newMatchups); // Skip initial double-byes
   }, []);
 
   useEffect(() => {
@@ -36,65 +36,54 @@ export default function VoteGame({ bracket }: Props) {
     startNewRound(generatedMatchups);
   }, [bracket.videos, startNewRound]);
 
-  useEffect(() => {
-    if (winners.length > 1) {
-      const generatedMatchups = generateMatchups(winners);
-      startNewRound(generatedMatchups);
-    } else if (winners.length === 1) {
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      setFinalWinner(winners[0]!);
-    }
-  }, [startNewRound, winners]);
-
   const currentMatchup = matchups[currentMatchupIndex];
 
-  const moveToNextMatchup = (startIndex: number, matchupList: Matchup[]) => {
-    let nextIndex = startIndex;
-
-    while (
-      nextIndex < matchupList.length &&
-      matchupList[nextIndex]?.a === null &&
-      matchupList[nextIndex]?.b === null
-    ) {
-      nextIndex += 1;
-    }
-
-    if (nextIndex < matchupList.length) {
-      setCurrentMatchupIndex(nextIndex);
-    } else {
-      const currentWinners = getWinners(matchupList);
-      setWinners(currentWinners);
-
-      if (currentWinners.length === 1) {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        setFinalWinner(currentWinners[0]!);
-      }
-    }
-  };
-
   const handleVote = (winner: Video) => {
-    if (currentMatchup) {
-      const updatedMatchup = { ...currentMatchup, winner };
-      const updatedMatchups = matchups.map((matchup, index) =>
-        index === currentMatchupIndex ? updatedMatchup : matchup,
-      );
-      setMatchups(updatedMatchups);
-      moveToNextMatchup(currentMatchupIndex + 1, updatedMatchups);
-    } else {
-      console.log("All matchups completed. No further votes allowed.");
-    }
+    if (!currentMatchup) return;
+
+    console.log("winner: ", winner);
+
+    const updatedMatchup = { ...currentMatchup, winner };
+
+    console.log(updatedMatchup);
+
+    const updatedMatchups = matchups.map((matchup, index) =>
+      index === currentMatchupIndex ? updatedMatchup : matchup,
+    );
+
+    console.log(updatedMatchups);
+
+    setMatchups(updatedMatchups);
+    // moveToNextMatchup(currentMatchupIndex + 1, updatedMatchups);
   };
 
-  const isFinalRound = matchups.length === 1;
+  // const moveToNextMatchup = (startIndex: number, matchupList: Matchup[]) => {
+  //   let nextIndex = startIndex;
 
-  console.log("currentMatchup: ", currentMatchup);
-  console.log("matchups: ", matchups);
+  //   while (
+  //     nextIndex < matchupList.length &&
+  //     matchupList[nextIndex]?.a === null &&
+  //     matchupList[nextIndex]?.b === null
+  //   ) {
+  //     nextIndex += 1;
+  //   }
+
+  //   if (nextIndex < matchupList.length) {
+  //     setCurrentMatchupIndex(nextIndex);
+  //   } else {
+  //     const currentWinners = getWinners(matchupList);
+  //     setWinners(currentWinners);
+
+  //     if (currentWinners.length === 1) {
+  //       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  //       setFinalWinner(currentWinners[0]!);
+  //     }
+  //   }
+  // };
 
   return (
     <div className="mt-20 flex flex-col items-center justify-center gap-6">
-      <div className="text-2xl font-semibold">
-        Round: {isFinalRound ? "Final" : currentRound}
-      </div>
+      <div className="text-2xl font-semibold">Round: {currentRound}</div>
       {finalWinner ? (
         <div className="flex flex-col items-center justify-center gap-3">
           <YoutubePlayer id={finalWinner.videoId} />
@@ -108,8 +97,8 @@ export default function VoteGame({ bracket }: Props) {
                 <div className="flex flex-col gap-3">
                   <YoutubePlayer id={currentMatchup.a.videoId} />
                   <p>{currentMatchup.a.videoId}</p>
-                  {/* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */}
-                  <Button onClick={() => handleVote(currentMatchup.a!)}>
+
+                  <Button onClick={() => handleVote(currentMatchup.a)}>
                     Vote A
                   </Button>
                 </div>
@@ -123,8 +112,8 @@ export default function VoteGame({ bracket }: Props) {
                 <div className="flex flex-col gap-3">
                   <YoutubePlayer id={currentMatchup.b.videoId} />
                   <p>{currentMatchup.b.videoId}</p>
-                  {/* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */}
-                  <Button onClick={() => handleVote(currentMatchup.b!)}>
+
+                  <Button onClick={() => handleVote(currentMatchup.b)}>
                     Vote B
                   </Button>
                 </div>
