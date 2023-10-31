29bebb50ef846aec21dd17f4ff8f511bd8272cd7
Brandon <93879641+beanziii@users.noreply.github.com>
2023-10-31 12:24:35 +0000

    bracket preview list now scrollable

--- src/components/features/bracket-preview/bracket-preview.tsx
+++ src/components/features/bracket-preview/bracket-preview.tsx
@@ -23,7 +23,7 @@ export default function BracketPreview({ bracket }: Props) {
   return (
     <>
       <div className="grid md:grid-cols-3">
-        <div className="">
+        <div className="mb-5">
           <Header
             title={bracket.name || "Could not find bracket name..."}
             description={
@@ -38,27 +38,29 @@ export default function BracketPreview({ bracket }: Props) {
           </Button>
         </div>
 
-        <ul className="col-span-2 space-y-5">
-          {bracket.videos.map((video) => {
-            return (
-              <li
-                key={video.id}
-                className="border-b-2 border-secondary pb-3 last:border-none"
-              >
-                {video.thumbnail && video.title && (
-                  <Image
-                    src={video.thumbnail}
-                    width={500}
-                    height={500}
-                    alt={`Youtube thumbnail for ${video.title}`}
-                    className="mb-2 h-auto w-auto"
-                  />
-                )}
-                <h3 className="font-semibold">{video.title}</h3>
-              </li>
-            );
-          })}
-        </ul>
+        <div className="col-span-2 md:max-h-screen md:overflow-scroll">
+          <ul className="space-y-5">
+            {bracket.videos.map((video) => {
+              return (
+                <li
+                  key={video.id}
+                  className="border-b-2 border-secondary pb-3 last:border-none"
+                >
+                  {video.thumbnail && video.title && (
+                    <Image
+                      src={video.thumbnail}
+                      width={500}
+                      height={500}
+                      alt={`Youtube thumbnail for ${video.title}`}
+                      className="mb-2 h-auto w-auto"
+                    />
+                  )}
+                  <h3 className="font-semibold">{video.title}</h3>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </>
   );
