2687cae19d04fb6b9e5be7a335b3fcec552e2383
Brandon <93879641+beanziii@users.noreply.github.com>
2023-12-22 21:19:53 +0000

    change alert to drawer on form page

--- src/pages/create-bracket.tsx
+++ src/pages/create-bracket.tsx
@@ -1,10 +1,20 @@
-import { Info } from "lucide-react";
+import { HelpCircle } from "lucide-react";
 import { useSession } from "next-auth/react";
 
 import CreateBracketForm from "@/components/features/bracket-form/create-bracket-form";
 import Header from "@/components/features/header/header";
 import Meta from "@/components/features/meta/meta";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 
 export default function CreatePage() {
   const { data: session } = useSession();
@@ -20,17 +30,31 @@ export default function CreatePage() {
         description="You can create a bracket here using a valid YouTube playlist link."
       />
       <CreateBracketForm />
-      <div className="fixed bottom-5 left-0 w-full pl-4 pr-4">
-        <Alert className="">
-          <Info className="h-4 w-4" />
-          <AlertTitle>Heads up</AlertTitle>
-          <AlertDescription>
-            Your YouTube playlist should be set to{" "}
-            <span className="font-bold">public</span> and include{" "}
-            <span className="font-bold">exactly 2, 4, 8, 16, 32, or 64</span>{" "}
-            videos.
-          </AlertDescription>
-        </Alert>
+
+      <div className="mt-4">
+        <Drawer>
+          <DrawerTrigger>
+            <HelpCircle />
+          </DrawerTrigger>
+          <DrawerContent>
+            <DrawerHeader>
+              <DrawerTitle>What is a valid playlist?</DrawerTitle>
+              <DrawerDescription>
+                Your YouTube playlist should be set to{" "}
+                <span className="font-bold">public</span> and include{" "}
+                <span className="font-bold">
+                  exactly 2, 4, 8, 16, 32, or 64
+                </span>{" "}
+                videos.
+              </DrawerDescription>
+            </DrawerHeader>
+            <DrawerFooter>
+              <DrawerClose>
+                <Button variant="outline">Close</Button>
+              </DrawerClose>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
       </div>
     </>
   );
