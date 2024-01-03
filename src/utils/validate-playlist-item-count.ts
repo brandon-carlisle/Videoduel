e19d76720418318444ef4380259a9bb65ebb8860
beanziii <93879641+beanziii@users.noreply.github.com>
2024-01-03 22:07:07 +0000

    refactor playlist count validator

--- src/utils/validate-playlist-item-count.ts
+++ src/utils/validate-playlist-item-count.ts
@@ -1,18 +1,3 @@
 export function validatePlaylistItemCount<T>(playlist: T[]): boolean {
-  switch (playlist.length) {
-    case 2:
-      return true;
-    case 4:
-      return true;
-    case 8:
-      return true;
-    case 16:
-      return true;
-    case 32:
-      return true;
-    case 64:
-      return true;
-  }
-
-  return false;
+  return [2, 4, 8, 16, 32, 64].includes(playlist.length);
 }
