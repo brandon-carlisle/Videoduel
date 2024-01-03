a4e4090d7001ebd9eb3c4e8db1abfc7ba8d51cd0
beanziii <93879641+beanziii@users.noreply.github.com>
2024-01-03 23:31:25 +0000

    format test

--- __tests__/utils/validate-playlist-item-count.test.ts
+++ __tests__/utils/validate-playlist-item-count.test.ts
@@ -1,13 +1,13 @@
+import { expect, test } from "vitest";
+
 import { validatePlaylistItemCount } from "../../src/utils/validate-playlist-item-count";
-import {expect, test} from "vitest"
 
 test("expects array of length 7 to not be valid", () => {
-    const testArray = Array.from(Array(7).keys());
-    expect(validatePlaylistItemCount(testArray)).toBe(false)
-})
-
+  const testArray = Array.from(Array(7).keys());
+  expect(validatePlaylistItemCount(testArray)).toBe(false);
+});
 
 test("expects array of length 32 to be valid", () => {
-    const testArray = Array.from(Array(32).keys());
-    expect(validatePlaylistItemCount(testArray)).toBe(true)
-})
+  const testArray = Array.from(Array(32).keys());
+  expect(validatePlaylistItemCount(testArray)).toBe(true);
+});
